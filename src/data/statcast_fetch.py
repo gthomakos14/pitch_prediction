64da85cc3affab3b8c4675faaf837b35773c01eb
Glenn <Glenn Thomakos>
2026-05-21 08:30:19 -0400

    tweaked data storage subdirectory

--- src/data/statcast_fetch.py
+++ src/data/statcast_fetch.py
@@ -1,16 +1,20 @@
-from pybaseball import statcast, cache
+from pybaseball import statcast
 import datetime as dt
 import os
 from pathlib import Path
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_DIRECTORY = PROJECT_ROOT / "data"
+RAW_DIRECTORY = PROJECT_ROOT / "data/raw"
 
 
 def refresh_statcast_data(min_year=2023, override_refresh=False):
-    parquet_files = os.listdir(DATA_DIRECTORY)
+    parquet_files = os.listdir(RAW_DIRECTORY)
     if (not parquet_files) or override_refresh:
+        if not override_refresh:
+            print(f'No statcast data found. Refreshing going back to {min_year}...')
+        else:
+            print('Override received. Refreshing statcast parquets now...')
         max_year = dt.datetime.now().year
         for i in range(min_year, max_year+1):
             # Each year is about 770k records
@@ -18,5 +22,5 @@ def refresh_statcast_data(min_year=2023, override_refresh=False):
             current = statcast(start_dt=f'{i}-03-01',
                             end_dt=f'{i}-11-15',
                             verbose=False)
-            export_path = os.path.join(DATA_DIRECTORY, f'statcast_{i}.parquet')
+            export_path = os.path.join(RAW_DIRECTORY, f'statcast_{i}.parquet')
             current.to_parquet(export_path, index=False)
